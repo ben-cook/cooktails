1352b50fc8d6347b457cff2869347154f26c8754
Ben Cook <mcbeanoo@gmail.com>
2021-07-08 23:17:35 +1000

    add filtering open close functionality

--- src/components/SearchPage/SearchBar.tsx
+++ src/components/SearchPage/SearchBar.tsx
@@ -1,46 +1,61 @@
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, Paper, Typography } from "@material-ui/core";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import { makeStyles } from "@material-ui/core/styles";
+import { useState } from "react";
 
 const useStyles = makeStyles({
   searchGridContainer: { flexGrow: 1, width: "100%" },
   searchbar: { marginTop: "2vh", marginBottom: "2vh", width: "100%" },
   filterToggle: { margin: "auto", "&:hover": { cursor: "pointer" } },
-  resultsGridContainer: { flexGrow: 1 },
-  resultsGridItem: {},
 });
 
-const SearchBar = ({
-  onChangeHandler,
-}: {
+interface SearchBarProps {
   onChangeHandler: (e: object & { target: { value: string } }) => void;
-}) => {
+}
+
+const SearchBar = ({ onChangeHandler }: SearchBarProps) => {
   const classes = useStyles();
 
+  const [open, setOpen] = useState<boolean>(false);
+
+  const changeOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-      spacing={2}
-      className={classes.searchGridContainer}
-    >
-      <Grid item xs zeroMinWidth>
-        <TextField
-          className={classes.searchbar}
-          label="Search for a drink"
-          variant="outlined"
-          onChange={onChangeHandler}
-        />
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={2}
+        className={classes.searchGridContainer}
+      >
+        <Grid item xs zeroMinWidth>
+          <TextField
+            className={classes.searchbar}
+            label="Search for a drink"
+            variant="outlined"
+            onChange={onChangeHandler}
+          />
+        </Grid>
+        <Grid item container xs={2} sm={1}>
+          <FilterListIcon
+            className={classes.filterToggle}
+            onClick={() => changeOpen()}
+          />
+        </Grid>
       </Grid>
-      <Grid item container xs={2} sm={1}>
-        <FilterListIcon
-          className={classes.filterToggle}
-          onClick={() => console.log("click")}
-        />
-      </Grid>
-    </Grid>
+
+      {open && (
+        <Paper>
+          <Typography variant="h6" color="initial">
+            Filter by ingredient:
+          </Typography>
+        </Paper>
+      )}
+    </>
   );
 };
 
