d08261449675b00e7b2f50a53bf21e02cb8eb18d
Ben Cook <mcbeanoo@gmail.com>
2021-07-18 11:42:15 +1000

    add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,18 @@ import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo/client";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter basename="/">
-        <App />
-      </HashRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <HashRouter basename="/">
+          <App />
+        </HashRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
