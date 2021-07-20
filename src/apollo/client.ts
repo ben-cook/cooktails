5296e5258b57ffa771fb5f3105588c32c3961c90
Ben Cook <mcbeanoo@gmail.com>
2021-07-20 15:02:11 +1000

    change back to heroku

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const localhost = "http://localhost:4000/";
-// const heroku = "https://cooktails-graphql.herokuapp.com/";
+const heroku = "https://cooktails-graphql.herokuapp.com/";
 
 export const client = new ApolloClient({
-  uri: localhost,
+  uri: heroku,
   cache: new InMemoryCache(),
 });
